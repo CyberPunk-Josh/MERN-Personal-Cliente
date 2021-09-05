c4d39914f5f1be76516222daff2f39c9da07c52c
joshue <cristianjoshue@hotmail.com>
2021-09-04 23:04:06 -0500

    UPDATE: MenuWeb

--- src/pages/Admin/MenuSider/MenuSider.jsx
+++ src/pages/Admin/MenuSider/MenuSider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link, withRouter } from 'react-router-dom';
 import {Layout, Menu} from 'antd';
-import {HomeOutlined, UserOutlined } from '@ant-design/icons';
+import {HomeOutlined, UserOutlined, ReadOutlined } from '@ant-design/icons';
 
 
 // SCSS:
@@ -25,6 +25,12 @@ function MenuSider(props){
                     <Link to={'/admin/users'}>
                         <UserOutlined />
                         <span className='nav-text'>Users</span>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key='/admin/menu'>
+                    <Link to={'/admin/menu'}>
+                        <ReadOutlined />
+                        <span className='nav-text'>Menu</span>
                     </Link>
                 </Menu.Item>
             </Menu>
